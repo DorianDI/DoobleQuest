f1d1b1045429035f32fd01c8d0bfc9f50ece002f
dorian di <dorianroch292@gmail.com>
2026-01-07 11:23:49 +0100

    font + finition titre Accueil

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // fond transparent
+      statusBarIconBrightness: Brightness.light, // ANDROID → icônes blanches
+      statusBarBrightness: Brightness.dark, // iOS → texte blanc
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -31,11 +39,107 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF1D132E),
       body: Center(
         child: Column(
-          mainAxisAlignment: .center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text('DoobleQuest', style: TextStyle(fontFamily: 'Lalezar', fontSize: 40),),
+            const SizedBox(height: 60),
+            Stack(
+              children: [
+                Transform.translate(
+                  offset: const Offset(-8, 4),
+                  child: Text(
+                    'Dooble Quest',
+                    style: const TextStyle(
+                      fontFamily: 'Bangers',
+                      fontSize: 68,
+                      color: Color(0xFFF49A24),
+                    ),
+                  ),
+                ),
+                Text(
+                  'Dooble Quest',
+                  style: TextStyle(
+                    fontFamily: 'Bangers',
+                    fontSize: 68,
+                    foreground: Paint()
+                      ..style = PaintingStyle.stroke
+                      ..strokeWidth = 3
+                      ..color = Colors.black,
+                  ),
+                ),
+                const Text(
+                  'Dooble Quest',
+                  style: TextStyle(
+                    fontFamily: 'Bangers',
+                    fontSize: 68,
+                    color: Color(0xFF571D7D),
+                  ),
+                ),
+              ],
+            ),
+            Transform.translate(
+              offset: const Offset(-8, 4),
+              child: RichText(
+                text: const TextSpan(
+                  style: TextStyle(
+                    fontFamily: 'Caveat',
+                    fontSize: 36,
+                  ),
+                  children: [
+                    TextSpan(text: 'Are you a ', style: TextStyle(color: Color(0xFF7C8ED0))),
+                    TextSpan(text: 'looser ', style: TextStyle(color: Color(0xFFF49A24))),
+                    TextSpan(text: '?', style: TextStyle(color: Color(0xFF7C8ED0))),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 30,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 80,
+                  height: 2,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        Color(0x00F49A24), // transparent
+                        Color(0xFFF49A24), // plein
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Container(
+                  width: 20,
+                  height: 20,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFF49A24),
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Container(
+                  width: 80,
+                  height: 2,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        Color(0xFFF49A24),
+                        Color(0x00F49A24),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
